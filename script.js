fcbf4f6c24e211122689a2494f4826aed9f281e4
pranit ingole <pranitingole1@gmail.com>
2021-09-29 22:26:06 +0530

    done

--- script.js
+++ script.js
@@ -7,17 +7,18 @@ var url = "https://api.funtranslations.com/translate/ubbi-dubbi.json?"
 
 
 function getUrl(text) {
-    return url +"text=" + text
+  return url +"text=" + text
 }
 
 btntranslate.addEventListener("click", async () => {
 
-   let text = bananawords.value;
- await fetch(getUrl(text))
-        .then(res => res.json())
-        // .then(json=> outputbox.innerText = json.contents.translated)
-        .then(json=> console.log(json))
-        .catch(error => console.log(error))
+ let text = bananawords.value;
+await fetch(getUrl(text))
+      .then(res => res.json())
+  .then(json => {
+    var trantext = json.contents.translated
+    outputbox.innerHTML = trantext;
+      } )
+      .catch(error => console.log(error))
 
 });
-
